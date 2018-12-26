546b2ffd777a71ea4869591d506913fe31cb0df7
maslenitsa93 <maslenitsa93@gmail.com>
2018-12-26 20:35:45 +0300

    Implement technical specification #1019

--- libraries/chain/include/golos/chain/worker_proposal_objects.hpp
+++ libraries/chain/include/golos/chain/worker_proposal_objects.hpp
@@ -1,3 +1,4 @@
+//TODO: rename me to worker_objects.hpp
 #pragma once
 
 #include <golos/chain/database.hpp>
@@ -19,7 +20,7 @@ namespace golos { namespace chain {
 
         template <typename Constructor, typename Allocator>
         worker_proposal_object(Constructor&& c, allocator <Allocator> a)
-            : permlink(a) {
+            : permlink(a), approved_techspec_permlink(a) {
             c(*this);
         };
 
@@ -30,13 +31,40 @@ namespace golos { namespace chain {
         worker_proposal_type type;
         worker_proposal_state state;
         asset deposit;
-        id_type techspec_id; // TODO
+        account_name_type approved_techspec_author;
+        shared_string approved_techspec_permlink;
         account_name_type worker;
         time_point_sec work_beginning_time;
         uint8_t worker_payments_count;
         time_point_sec payment_beginning_time;
         time_point_sec created;
         time_point_sec modified;
+    };
+
+    class worker_techspec_object : public object<worker_techspec_object_type, worker_techspec_object> {
+    public:
+        worker_techspec_object() = delete;
+
+        template <typename Constructor, typename Allocator>
+        worker_techspec_object(Constructor&& c, allocator <Allocator> a)
+            : permlink(a), worker_proposal_permlink(a) {
+            c(*this);
+        };
+
+        id_type id;
+
+        account_name_type author;
+        shared_string permlink;
+        account_name_type worker_proposal_author;
+        shared_string worker_proposal_permlink;
+        time_point_sec created;
+        time_point_sec modified;
+        asset specification_cost;
+        uint32_t specification_eta;
+        asset development_cost;
+        uint32_t development_eta;
+        uint16_t payments_count;
+        uint32_t payments_interval;
     };
 
     struct by_permlink;
@@ -58,8 +86,40 @@ namespace golos { namespace chain {
                     chainbase::strcmp_less>>>,
         allocator<worker_proposal_object>>;
 
+    struct by_worker_proposal;
+
+    using worker_techspec_index = multi_index_container<
+        worker_techspec_object,
+        indexed_by<
+            ordered_unique<
+                tag<by_id>,
+                member<worker_techspec_object, worker_techspec_object_id_type, &worker_techspec_object::id>>,
+            ordered_unique<
+                tag<by_permlink>,
+                composite_key<
+                    worker_techspec_object,
+                    member<worker_techspec_object, account_name_type, &worker_techspec_object::author>,
+                    member<worker_techspec_object, shared_string, &worker_techspec_object::permlink>>,
+                composite_key_compare<
+                    std::less<account_name_type>,
+                    chainbase::strcmp_less>>,
+            ordered_non_unique<
+                tag<by_worker_proposal>,
+                composite_key<
+                    worker_techspec_object,
+                    member<worker_techspec_object, account_name_type, &worker_techspec_object::worker_proposal_author>,
+                    member<worker_techspec_object, shared_string, &worker_techspec_object::worker_proposal_permlink>>,
+                composite_key_compare<
+                    std::less<account_name_type>,
+                    chainbase::strcmp_less>>>,
+        allocator<worker_techspec_object>>;
+
 } } // golos::chain
 
 CHAINBASE_SET_INDEX_TYPE(
     golos::chain::worker_proposal_object,
     golos::chain::worker_proposal_index);
+
+CHAINBASE_SET_INDEX_TYPE(
+    golos::chain::worker_techspec_object,
+    golos::chain::worker_techspec_index);
--- libraries/protocol/include/golos/protocol/worker_proposal_operations.hpp
+++ libraries/protocol/include/golos/protocol/worker_proposal_operations.hpp
@@ -1,5 +1,7 @@
+//TODO: rename me to worker_operations.hpp
 #pragma once
 
+#include <golos/protocol/asset.hpp>
 #include <golos/protocol/authority.hpp>
 #include <golos/protocol/base.hpp>
 
@@ -38,6 +40,40 @@ namespace golos { namespace protocol {
         }
     };
 
+    struct worker_techspec_operation : public base_operation {
+        account_name_type author;
+        std::string permlink;
+        account_name_type worker_proposal_author;
+        std::string worker_proposal_permlink;
+        asset specification_cost;
+        uint32_t specification_eta;
+        asset development_cost;
+        uint32_t development_eta;
+        uint16_t payments_count;
+        uint32_t payments_interval;
+
+        extensions_type extensions;
+
+        void validate() const;
+
+        void get_required_posting_authorities(flat_set<account_name_type>& a) const {
+            a.insert(author);
+        }
+    };
+
+    struct worker_techspec_delete_operation : public base_operation {
+        account_name_type author;
+        std::string permlink;
+
+        extensions_type extensions;
+
+        void validate() const;
+
+        void get_required_posting_authorities(flat_set<account_name_type>& a) const {
+            a.insert(author);
+        }
+    };
+
 } } // golos::protocol
 
 FC_REFLECT_ENUM(golos::protocol::worker_proposal_type, (task)(premade_work)(_wpt_size))
@@ -47,4 +83,13 @@ FC_REFLECT(
 
 FC_REFLECT(
     (golos::protocol::worker_proposal_delete_operation),
+    (author)(permlink)(extensions))
+
+FC_REFLECT(
+    (golos::protocol::worker_techspec_operation),
+    (author)(permlink)(worker_proposal_author)(worker_proposal_permlink)(specification_cost)(specification_eta)
+    (development_cost)(development_eta)(payments_count)(payments_interval)(extensions))
+
+FC_REFLECT(
+    (golos::protocol::worker_techspec_delete_operation),
     (author)(permlink)(extensions))
